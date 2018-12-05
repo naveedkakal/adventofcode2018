fdc686d1823f696917498f62a2e74d9b5ce64b31
Naveed Kakal <naveed.kakal@gmail.com>
2018-12-05 08:01:10 -0600

    part 1 done

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -1,18 +1,27 @@
 defmodule Day2 do
-  @moduledoc """
-  Documentation for Day2.
-  """
 
-  @doc """
-  Hello world.
+  def go do
+    {:ok, data} = File.read("lib/input_1.txt")
+    compute(data)
+  end
 
-  ## Examples
+  def compute(data) do
+    {twices, thrices} =
+      data
+      |> String.split("\n")
+      |> Enum.map(fn x -> String.graphemes(x) end)
+      |> Enum.reduce({0, 0}, fn x, {two_ct, three_ct} ->
+        word_dat =
+          Enum.reduce(x, %{}, fn y, acc2 ->
+            Map.update(acc2, y, 1, fn cur -> cur + 1 end)
+          end)
+          |> Map.values()
 
-      iex> Day2.hello()
-      :world
+        twice = if Enum.any?(word_dat, &(&1 == 2)), do: two_ct + 1, else: two_ct
+        thrice = if Enum.any?(word_dat, &(&1 == 3)), do: three_ct + 1, else: three_ct
+        {twice, thrice}
+      end)
 
-  """
-  def hello do
-    :world
+    twices * thrices
   end
 end
--- day2/test/day2_test.exs
+++ day2/test/day2_test.exs
@@ -2,7 +2,9 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Day2
 
-  test "greets the world" do
-    assert Day2.hello() == :world
+  test "computes frequency" do
+    {:ok, data} = File.read("test/test_input_1.txt")
+
+    assert Day2.compute(data) == 12
   end
 end
