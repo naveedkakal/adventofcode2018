db1c47bfb4e8b7b9e5a5590ac714f361673c5097
Naveed Kakal <naveed.kakal@gmail.com>
2018-12-05 21:06:10 -0600

    Part 1 done

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -1,18 +1,34 @@
 defmodule Day3 do
-  @moduledoc """
-  Documentation for Day3.
-  """
+  def popular_squares(data) do
+    data
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.reduce(%{}, fn elf_square, acc ->
+      build_squares(elf_square)
+      |> Enum.reduce(acc, fn z, acc2 ->
+        Map.update(acc2, z, 1, fn cv -> cv + 1 end)
+      end)
+    end)
+    |> Enum.filter(fn {_, v} -> v >= 2 end)
+    |> Enum.count
+  end
 
-  @doc """
-  Hello world.
+  # Pretty sure this parsing should be done via regex
+  # Still suck at those - doing string splits trims /shrug
+  def build_squares(data \\ "#1 @ 1,3: 4x4" ) do
+    [_elf_id, _sym, front_raw, back_raw] = String.split(data)
+    [x0, y0] = front_raw |> String.trim(":") |> String.split(",")
+    [dx, dy] = back_raw |> String.split("x")
+    x0 = String.to_integer(x0)
+    dx = String.to_integer(dx)
+    x_range = x0..(x0 + dx - 1)
 
-  ## Examples
+    y0 = String.to_integer(y0)
+    dy = String.to_integer(dy)
+    y_range = y0..(y0 + dy - 1)
 
-      iex> Day3.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    Enum.reduce(x_range, [], fn x, acc ->
+      acc ++ Enum.map(y_range, fn y -> {x, y} end)
+    end)
   end
 end
--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -2,7 +2,10 @@ defmodule Day3Test do
   use ExUnit.Case
   doctest Day3
 
-  test "greets the world" do
-    assert Day3.hello() == :world
+
+  test "finds popular squares" do
+    {:ok, data} = File.read("test/test_input_1.txt")
+
+    assert Day3.popular_squares(data) == 4
   end
 end
